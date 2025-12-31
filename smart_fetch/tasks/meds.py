8c2ec6d5b6f647160df44fdf7a47602fecd8d22b
Michael Terry <michael.terry@childrens.harvard.edu>
2025-12-31 09:31:01 -0500

    hydrate: speed up PractitionerRole searching on second time
    If interrupted and we need to re-hydrate PractitionerRoles, first look
    to see which Practitioners we already searched for.
    
    This works by separating out the searched Roles from the manually
    referenced Roles, so we know which is which.
    
    Also, add a little more output to hydration tasks, to make it clearer
    what's happening.

--- smart_fetch/tasks/meds.py
+++ smart_fetch/tasks/meds.py
@@ -1,34 +1,12 @@
 from smart_fetch import hydrate_utils, resources
 
 
-class MedsTask(hydrate_utils.Task):
+class MedsTask(hydrate_utils.ReferenceDownloadTask):
     NAME = "meds"
     INPUT_RES_TYPE = resources.MEDICATION_REQUEST
     OUTPUT_RES_TYPE = resources.MEDICATION
-
-    async def run(self, workdir: str, source_dir: str | None = None, **kwargs) -> None:
-        stats = await hydrate_utils.process(
-            task_name=self.NAME,
-            desc="Downloading",
-            workdir=workdir,
-            source_dir=source_dir or workdir,
-            input_type=self.INPUT_RES_TYPE,
-            output_type=self.OUTPUT_RES_TYPE,
-            callback=self.process_one,
-            compress=self.compress,
-        )
-        if stats:
-            stats.print("downloaded", f"{self.OUTPUT_RES_TYPE}s")
-
-    async def process_one(
-        self, resource: dict, id_pool: set[str], **kwargs
-    ) -> hydrate_utils.Result:
-        med_ref = resource.get("medicationReference", {}).get("reference")
-        return [
-            await hydrate_utils.download_reference(
-                self.client, id_pool, med_ref, self.OUTPUT_RES_TYPE
-            )
-        ]
+    REFS = ("medicationReference",)
+    FILE_SLUG = None  # only one Medication source, don't need to differentiate
 
 
 MEDICATION_TASKS = [MedsTask]
--- smart_fetch/tasks/obs.py
+++ smart_fetch/tasks/obs.py
@@ -3,71 +3,22 @@ import abc
 from smart_fetch import hydrate_utils, resources
 
 
-class ObsTask(hydrate_utils.Task, abc.ABC):
+class ObsTask(hydrate_utils.ReferenceDownloadTask, abc.ABC):
     OUTPUT_RES_TYPE = resources.OBSERVATION
 
 
 class DxrResultsTask(ObsTask):
     NAME = "dxr-results"
     INPUT_RES_TYPE = resources.DIAGNOSTIC_REPORT
-
-    async def run(self, workdir: str, source_dir: str | None = None, **kwargs) -> None:
-        stats = await hydrate_utils.process(
-            task_name=self.NAME,
-            desc="Downloading result",
-            workdir=workdir,
-            source_dir=source_dir or workdir,
-            input_type=self.INPUT_RES_TYPE,
-            output_type=self.OUTPUT_RES_TYPE,
-            callback=self.process_one,
-            file_slug="results",
-            compress=self.compress,
-        )
-        if stats:
-            stats.print("downloaded", f"{self.INPUT_RES_TYPE}s", f"Result {self.OUTPUT_RES_TYPE}s")
-
-    async def process_one(
-        self, resource: dict, id_pool: set[str], **kwargs
-    ) -> hydrate_utils.Result:
-        return [
-            await hydrate_utils.download_reference(
-                self.client, id_pool, result.get("reference"), self.OUTPUT_RES_TYPE
-            )
-            for result in resource.get("result", [])
-        ]
+    REFS = ("result*",)
+    FILE_SLUG = "results"
 
 
 class ObsMembersTask(ObsTask):
     NAME = "obs-members"
     INPUT_RES_TYPE = resources.OBSERVATION
-
-    async def run(self, workdir: str, source_dir: str | None = None, **kwargs) -> None:
-        stats = await hydrate_utils.process(
-            task_name=self.NAME,
-            desc="Downloading member",
-            workdir=workdir,
-            source_dir=source_dir or workdir,
-            input_type=self.INPUT_RES_TYPE,
-            callback=self.process_one,
-            file_slug="members",
-            compress=self.compress,
-        )
-        if stats:
-            stats.print("downloaded", f"{resources.OBSERVATION}s", "Members")
-
-    async def process_one(
-        self, resource: dict, id_pool: set[str], **kwargs
-    ) -> hydrate_utils.Result:
-        results = [
-            await hydrate_utils.download_reference(
-                self.client, id_pool, member.get("reference"), self.OUTPUT_RES_TYPE
-            )
-            for member in resource.get("hasMember", [])
-        ]
-        for result in results:
-            if result[0]:
-                results.extend(await self.process_one(result[0], id_pool))
-        return results
+    REFS = ("hasMember*",)
+    FILE_SLUG = "members"
 
 
 OBSERVATION_TASKS = [DxrResultsTask, ObsMembersTask]
